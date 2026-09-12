3a796b4d87fbc7a506887744dc9f60f30df2ee4f
i.kalashnikov <i.kalashnikov@campus.fct.unl.pt>
2026-09-12 15:58:12 +0100

    massive restructure, extracted model logic in separate class, configs

---
+++ model.py
@@ -0,0 +1,107 @@
+import math
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
+from torchtune.modules import RotaryPositionalEmbeddings
+
+
+@dataclass
+class ModelConfig:
+    vocab_size: int
+    seq_len: int = 2048
+    d_model: int = 768
+    d_head: int = 64
+    n_heads: int = 12 
+    n_kv_heads: int = 4
+    n_layers: int = 16
+
+    @property
+    def d_ff(self) -> int:
+        return int(self.d_model * 8 / 3) # 2048
+
+
+class TransformerBlock(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.config = config
+
+        self.atn_norm = nn.RMSNorm(config.d_model, eps=1e-5)  # https://docs.pytorch.org/docs/2.13/generated/torch.nn.RMSNorm.html, eps for bfloat stability
+        self.q = nn.Linear(config.d_model, config.d_head * config.n_heads, bias=False)
+        self.k = nn.Linear(config.d_model, config.d_head * config.n_kv_heads, bias=False)
+        self.v = nn.Linear(config.d_model, config.d_head * config.n_kv_heads, bias=False)
+        self.o = nn.Linear(config.d_model, config.d_model, bias=False)
+
+        self.q_norm = nn.RMSNorm(config.d_head, eps=1e-5)
+        self.k_norm = nn.RMSNorm(config.d_head, eps=1e-5) 
+        self.pre_ff_norm = nn.RMSNorm(config.d_model, eps=1e-5)
+
+        self.w1 = nn.Linear(config.d_model, config.d_ff, bias=False)
+        self.w2 = nn.Linear(config.d_ff, config.d_model, bias=False)
+        self.w3 = nn.Linear(config.d_model, config.d_ff, bias=False)
+
+        std = 0.02
+        nn.init.normal_(self.q.weight, mean=0.0, std=std)
+        nn.init.normal_(self.k.weight, mean=0.0, std=std)
+        nn.init.normal_(self.v.weight, mean=0.0, std=std)
+        nn.init.normal_(self.w1.weight, mean=0.0, std=std)
+        nn.init.normal_(self.w3.weight, mean=0.0, std=std)
+
+        scaled_std = std / math.sqrt(2 * config.n_layers)  # for residual projections 
+        nn.init.normal_(self.o.weight, mean=0.0, std=scaled_std)
+        nn.init.normal_(self.w2.weight, mean=0.0, std=scaled_std)
+
+
+    def forward(self, x, rope):
+        b, s, _ = x.shape
+
+        x_norm = self.atn_norm(x)
+
+        Q = self.q_norm(self.q(x_norm).view(b, s, self.config.n_heads, self.config.d_head))
+        K = self.k_norm(self.k(x_norm).view(b, s, self.config.n_kv_heads, self.config.d_head))
+        V = self.v(x_norm).view(b, s, self.config.n_kv_heads, self.config.d_head)
+
+        Q_rotated = rope(Q)
+        K_rotated = rope(K)
+
+        x_atn = F.scaled_dot_product_attention(
+            Q_rotated.transpose(1, 2), K_rotated.transpose(1, 2), V.transpose(1, 2), is_causal=True, enable_gqa=True)  # https://docs.pytorch.org/docs/2.13/generated/torch.nn.functional.scaled_dot_product_attention.html
+
+        x_atn = self.o(x_atn.transpose(1, 2).reshape(b, s, self.config.d_head * self.config.n_heads))
+
+        x = x + x_atn
+
+        x_norm = self.pre_ff_norm(x)
+
+        swiglu = self.w2(self.w1(x_norm) * F.silu(self.w3(x_norm)))
+
+        x = x + swiglu
+        return x
+
+
+
+class Model(nn.Module):
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+        self.config = config
+
+        self.lm_head = nn.Embedding(config.vocab_size, config.d_model)
+        self.rope = RotaryPositionalEmbeddings(dim=config.d_head, max_seq_len=config.seq_len + 1)  # https://meta-pytorch.org/torchtune/stable/generated/torchtune.modules.RotaryPositionalEmbeddings.html?highlight=rope
+        self.post_ff_norm = nn.RMSNorm(config.d_model, eps=1e-5)
+        self.ff = nn.ModuleList([TransformerBlock(config) for _ in range(config.n_layers)])
+
+        nn.init.normal_(self.lm_head.weight, mean=0.0, std=0.02)
+
+
+    def forward(self, tokens):
+        x = self.lm_head(tokens)
+
+        for block in self.ff:
+            #x = block(x, self.rope)
+            x = checkpoint(block, x, self.rope, use_reentrant=False)  # 15% tok/s gain
+        x = self.post_ff_norm(x)
+
+        return x
--- train.py
+++ train.py
@@ -1,251 +1,220 @@
+from dataclasses import dataclass
 import math
 import time
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.optim.lr_scheduler import LambdaLR
-from torch.utils.checkpoint import checkpoint
 from torch.utils.data import DataLoader, IterableDataset
-from torchdata.stateful_dataloader import StatefulDataLoader
 
 from datasets import load_dataset
 from liger_kernel.transformers.fused_linear_cross_entropy import LigerFusedLinearCrossEntropyLoss
-from torchtune.modules import RotaryPositionalEmbeddings
 from transformers import AutoTokenizer
 
 import storage
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-torch.set_float32_matmul_precision('high')
-repo_id = "kalashnikov-dev/miniLM"
+from model import Model, ModelConfig
 
 
-tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M")
+@dataclass
+class TrainConfig:
+    batch_size: int = 64
+    accumulation_steps: int = 2
+    total_steps: int = 105000
+    warmup_steps: int = 2000
+    lr: float = 2e-3
+    repo_id: str = "kalashnikov-dev/miniLM"
 
+    @property
+    def total_micro_steps(self):
+        return self.total_steps * self.accumulation_steps
 
-vocab_size = len(tokenizer)
-batch_size = 64
-seq_len = 2048
-d_model = 768
-d_ff = int(d_model * 8/3) # 2048
-d_head = 64
-n_heads = 12 
-n_kv_heads = 4
-n_layers = 16
-lr = 2e-3
-accumulation_steps = 2
-total_steps = 115000
-total_micro_steps = total_steps * accumulation_steps
-warmup_steps = 2000
-decay_steps = int(total_steps * 0.20)
-stable_end = total_steps - decay_steps 
+    @property
+    def decay_steps(self):
+        return int(self.total_steps * 0.20)
 
-
-
-
-class TransformerBlock(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.atn_norm = nn.RMSNorm(d_model, eps=1e-5) # https://docs.pytorch.org/docs/2.13/generated/torch.nn.RMSNorm.html, eps for bfloat stability
-        self.q = nn.Linear(d_model, d_head * n_heads, bias = False)
-        self.k = nn.Linear(d_model, d_head * n_kv_heads, bias = False)
-        self.v = nn.Linear(d_model, d_head * n_kv_heads, bias = False)
-        self.o = nn.Linear(d_model, d_model, bias = False)
-
-        self.q_norm = nn.RMSNorm(d_head, eps=1e-5)
-        self.k_norm = nn.RMSNorm(d_head, eps=1e-5) 
-        self.pre_ff_norm = nn.RMSNorm(d_model, eps=1e-5)
-
-        self.w1 = nn.Linear(d_model, d_ff, bias = False)
-        self.w2 = nn.Linear(d_ff, d_model, bias = False)
-        self.w3 = nn.Linear(d_model, d_ff, bias = False)
-
-        std = 0.02
-        nn.init.normal_(self.q.weight, mean=0.0, std=std)
-        nn.init.normal_(self.k.weight, mean=0.0, std=std)
-        nn.init.normal_(self.v.weight, mean=0.0, std=std)
-        nn.init.normal_(self.w1.weight, mean=0.0, std=std)
-        nn.init.normal_(self.w3.weight, mean=0.0, std=std)
-
-        scaled_std = std / math.sqrt(2 * n_layers) # for residual projections 
-        nn.init.normal_(self.o.weight, mean=0.0, std=scaled_std)
-        nn.init.normal_(self.w2.weight, mean=0.0, std=scaled_std)
-
-
-    def forward(self, x, rope):
-        b, s, _ = x.shape
-
-        x_norm = self.atn_norm(x)
-
-        Q = self.q_norm(self.q(x_norm).view(b, s, n_heads, d_head))
-        K = self.k_norm(self.k(x_norm).view(b, s, n_kv_heads, d_head))
-        V = self.v(x_norm).view(b, s, n_kv_heads, d_head)
-
-        Q_rotated = rope(Q)
-        K_rotated = rope(K)
-
-        x_atn = F.scaled_dot_product_attention(
-            Q_rotated.transpose(1, 2), K_rotated.transpose(1, 2), V.transpose(1, 2), is_causal = True, enable_gqa = True) #  https://docs.pytorch.org/docs/2.13/generated/torch.nn.functional.scaled_dot_product_attention.html
-
-        x_atn = self.o(x_atn.transpose(1, 2).reshape(b, s, d_head * n_heads))
-
-        x = x + x_atn
-
-        x_norm = self.pre_ff_norm(x)
-
-        swiglu = self.w2(self.w1(x_norm) * F.silu(self.w3(x_norm)))
-
-        x = x + swiglu
-        return x
-
-
-
-class Model(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.lm_head = nn.Embedding(vocab_size, d_model)
-        self.rope = RotaryPositionalEmbeddings(dim = d_head, max_seq_len = seq_len+1) # https://meta-pytorch.org/torchtune/stable/generated/torchtune.modules.RotaryPositionalEmbeddings.html?highlight=rope
-        self.post_ff_norm = nn.RMSNorm(d_model, eps=1e-5)
-        self.ff = nn.ModuleList([TransformerBlock() for _ in range(n_layers)])
-
-        nn.init.normal_(self.lm_head.weight, mean=0.0, std=0.02)
-
-
-    def forward(self, tokens):
-        x = self.lm_head(tokens)
-
-        for block in self.ff:
-            #x = block(x, self.rope)
-            x = checkpoint(block, x, self.rope, use_reentrant=False) # 15% tok/s gain
-        x = self.post_ff_norm(x)
-
-        return x
-
+    @property
+    def stable_end(self):
+        return self.total_steps - self.decay_steps 
 
 
 
 class StreamingDataset(IterableDataset):
-    def __init__(self, stream):
+    
+    def __init__(self, stream, tokenizer, seq_len, batch_size):
         self.stream = stream
+        self.tokenizer = tokenizer
+        self.seq_len = seq_len
+        self.batch_size = batch_size
 
     def __iter__(self):
         buf = []
         batch = []
         for x in self.stream:
-            tokens = tokenizer.encode(x["text"], add_special_tokens=False)
+            tokens = self.tokenizer.encode(x["text"], add_special_tokens=False)
             buf.extend(tokens)
-            buf.append(tokenizer.eos_token_id)
+            buf.append(self.tokenizer.eos_token_id)
                 
-            while len(buf) >= seq_len + 1:
-                chunk = buf[:seq_len + 1]
-                buf = buf[seq_len:]
+            while len(buf) >= self.seq_len + 1:
+                chunk = buf[:self.seq_len + 1]
+                buf = buf[self.seq_len:]
                 batch.append(torch.tensor(chunk))
 
-                if len(batch) == batch_size:
+                if len(batch) == self.batch_size:
                     yield torch.stack(batch)
                     batch = []
 
 
+class LRMultiplier:
+    def __init__(self, warmup_steps, stable_end, decay_steps):
+        self.warmup_steps = warmup_steps
+        self.stable_end = stable_end
+        self.decay_steps = decay_steps
+
+    def __call__(self, step):
+        if step < self.warmup_steps:
+            return (step + 1) / self.warmup_steps
+        if step < self.stable_end:
+            return 1.0
+        p = min((step - self.stable_end) / self.decay_steps, 1.0)
+        return max(0.0, 1.0 - math.sqrt(p))
 
 
-
-def get_lr_multiplier(step):
-    if step < warmup_steps:
-        return (step + 1) / warmup_steps
-    if step < stable_end:
-        return 1.0
-    p = min((step - stable_end) / decay_steps, 1.0)
-    return max(0.0, 1.0 - math.sqrt(p))
+def configure_device():
+    torch.set_float32_matmul_precision('high')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return device
 
 
-raw_model = Model().to(device)
-
-decay_params = [p for p in raw_model.parameters() if p.requires_grad and p.dim() >= 2]
-no_decay_params = [p for p in raw_model.parameters() if p.requires_grad and p.dim() < 2]
-
-optimizer = torch.optim.AdamW(
-    [{'params': decay_params, 'weight_decay': 0.1},{'params': no_decay_params, 'weight_decay': 0.0}],
-    lr=lr, betas=(0.9, 0.95), eps=1e-8, fused=True
-)
-scheduler = LambdaLR(optimizer, lr_lambda=get_lr_multiplier)
-fused_ce = LigerFusedLinearCrossEntropyLoss()
-
-resume_micro_step = 0
-docs_to_skip = 0
-ckpt = storage.load_checkpoint(device)
-if ckpt:
-    raw_model.load_state_dict(ckpt['model_state_dict'])
-    optimizer.load_state_dict(ckpt['optimizer_state_dict'])
-    scheduler.load_state_dict(ckpt['scheduler_state_dict'])
-    resume_step = ckpt['step']
-    resume_micro_step = resume_step * accumulation_steps
-
-    tokens_per_step = batch_size * seq_len * accumulation_steps
-    tokens_seen = resume_step * tokens_per_step
-    AVG_TOKENS_PER_DOC = 1000 
-    docs_to_skip = int(tokens_seen / AVG_TOKENS_PER_DOC)
-    if docs_to_skip > 0:
-        print(f"skipping {docs_to_skip} documents...")
+def build_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M")
+    return tokenizer
 
 
-    print(f"loaded checkpoint {resume_step}")
+def build_configs(tokenizer):
+    train_config = TrainConfig()
+    model_config = ModelConfig(vocab_size=len(tokenizer))
+    return train_config, model_config
 
 
-stream = load_dataset(
-    "HuggingFaceFW/fineweb-edu", 
-    name="sample-100BT", 
-    split="train", 
-    streaming=True
-).skip(docs_to_skip).shuffle(seed=1337, buffer_size=10_000)
+def build_rawmodel(model_config, device):
+    raw_model = Model(model_config).to(device)
+    return raw_model
 
 
-dataset = StreamingDataset(stream)
-dataloader = DataLoader(dataset, batch_size=None, pin_memory=True) 
+def build_optimizer(raw_model, train_config):
+    decay_params = [p for p in raw_model.parameters() if p.requires_grad and p.dim() >= 2]
+    no_decay_params = [p for p in raw_model.parameters() if p.requires_grad and p.dim() < 2]
+    optimizer = torch.optim.AdamW(
+        [{'params': decay_params, 'weight_decay': 0.1}, {'params': no_decay_params, 'weight_decay': 0.0}],
+        lr=train_config.lr, betas=(0.9, 0.95), eps=1e-8, fused=True
+    )
+    return optimizer
 
-model = torch.compile(raw_model)
+
+def build_scheduler(optimizer, train_config):
+    scheduler = LambdaLR(optimizer, lr_lambda=LRMultiplier(train_config.warmup_steps, train_config.stable_end, train_config.decay_steps))
+    return scheduler
 
 
-running_loss = 0.0
-t0 = time.perf_counter()
-for micro_step, batch in enumerate(dataloader, start=resume_micro_step):
-    if micro_step >= total_micro_steps: 
-        break
-    
-    batch = batch.to(device, non_blocking=True)
-    
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+def load_last_checkpoint(raw_model, device, optimizer, scheduler, train_config, model_config):
+    resume_micro_step = 0
+    docs_to_skip = 0
+    ckpt = storage.load_checkpoint(device)
+    if ckpt:
+        raw_model.load_state_dict(ckpt['model_state_dict'])
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        scheduler.load_state_dict(ckpt['scheduler_state_dict'])
+        resume_step = ckpt['step']
+        resume_micro_step = resume_step * train_config.accumulation_steps
+
+        tokens_per_step = train_config.batch_size * model_config.seq_len * train_config.accumulation_steps
+        tokens_seen = resume_step * tokens_per_step
+        AVG_TOKENS_PER_DOC = 1000 
+        docs_to_skip = int(tokens_seen / AVG_TOKENS_PER_DOC)
+
+        print(f"loaded checkpoint {resume_step}, skipped {docs_to_skip} documents")
+
+    return resume_micro_step, docs_to_skip
+
+
+def build_dataloader(docs_to_skip, tokenizer, train_config, model_config):
+    stream = load_dataset(
+        "HuggingFaceFW/fineweb-edu", 
+        name="sample-100BT", 
+        split="train", 
+        streaming=True
+    ).skip(docs_to_skip).shuffle(seed=1337, buffer_size=10_000)
+    dataset = StreamingDataset(stream, tokenizer, model_config.seq_len, train_config.batch_size)
+    dataloader = DataLoader(dataset, batch_size=None, pin_memory=True) 
+    return dataloader
+
+
+def build_model(raw_model):
+    model = torch.compile(raw_model)
+    return model
+
+
+def train(raw_model, model, train_config, model_config, device, dataloader, resume_micro_step, optimizer, scheduler, fused_ce):
+    running_loss = 0.0
+    t0 = time.perf_counter()
+    for micro_step, batch in enumerate(dataloader, start=resume_micro_step):
+        if micro_step >= train_config.total_micro_steps: 
+            break
         
-        pre_logits = model(batch[:, :-1])
-        targets = batch[:, 1:]
-
-        loss = fused_ce(raw_model.lm_head.weight, pre_logits.reshape(-1, d_model), targets.reshape(-1))
-        loss = loss / accumulation_steps
-
-    loss.backward()
-    running_loss += loss.detach()
-
-    if (micro_step + 1) % accumulation_steps == 0:
-        grad_norm = nn.utils.clip_grad_norm_(raw_model.parameters(), 1.0)
-        optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad(set_to_none=True)
-
-        step = (micro_step + 1) // accumulation_steps
-        if step % 250 == 0:
-            storage.save_checkpoint(raw_model, optimizer, scheduler, step, repo_id)
+        batch = batch.to(device, non_blocking=True)
         
-        #prints
-        n_tokens = batch_size * seq_len
-        dt = time.perf_counter() - t0
-        step_loss = running_loss.item()
-        print(f"step {step} loss={step_loss:.4f}  grad_norm={grad_norm.item():.2f}  {dt:.2f}s {n_tokens*accumulation_steps / dt:.0f} tok/s")
-        running_loss = 0.0
-        t0 = time.perf_counter()
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            
+            pre_logits = model(batch[:, :-1])
+            targets = batch[:, 1:]
+
+            loss = fused_ce(raw_model.lm_head.weight, pre_logits.reshape(-1, model_config.d_model), targets.reshape(-1))
+            loss = loss / train_config.accumulation_steps
+
+        loss.backward()
+        running_loss += loss.detach()
+
+        if (micro_step + 1) % train_config.accumulation_steps == 0:
+            grad_norm = nn.utils.clip_grad_norm_(raw_model.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            step = (micro_step + 1) // train_config.accumulation_steps
+            if step % 500 == 0:
+                storage.save_checkpoint(raw_model, optimizer, scheduler, step, train_config.repo_id)
+            
+            #prints
+            n_tokens = train_config.batch_size * model_config.seq_len
+            dt = time.perf_counter() - t0
+            step_loss = running_loss.item()
+            print(f"step {step} loss={step_loss:.4f}  grad_norm={grad_norm.item():.2f}  {dt:.2f}s {n_tokens*train_config.accumulation_steps / dt:.0f} tok/s")
+            running_loss = 0.0
+            t0 = time.perf_counter()
 
 
-storage.save_final_model(raw_model, repo_id)
-storage.wait_for_uploads()
 
+def main():
+    device = configure_device()
+    tokenizer = build_tokenizer()
+    train_config, model_config = build_configs(tokenizer)
+    raw_model = build_rawmodel(model_config, device)
+    optimizer = build_optimizer(raw_model, train_config)
+    scheduler = build_scheduler(optimizer, train_config)
+    fused_ce = LigerFusedLinearCrossEntropyLoss()
+    resume_micro_step, docs_to_skip = load_last_checkpoint(raw_model, device, optimizer, scheduler, train_config, model_config)
+    dataloader = build_dataloader(docs_to_skip, tokenizer, train_config, model_config)
+    model = build_model(raw_model)
+
+    train(
+        raw_model, model, train_config, model_config, 
+        device, dataloader, resume_micro_step, 
+        optimizer, scheduler, fused_ce
+    )
+    
+    storage.save_final_model(raw_model, train_config.repo_id)
+    storage.wait_for_uploads()
+
+
+if __name__ == "__main__":
+    main()
